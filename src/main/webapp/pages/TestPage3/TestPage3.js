17a3b01f581c0c79992fbf2ae1c69725befb851f
Sushma Wm <sushma.maddirala@wavemaker.com>
2017-10-05 11:43:12 +0000

    fr New files added - [TestPage,TestPage3,hrdb,data] Files modified - [Main,leftnav,profiles,pom.xml,project-services.xml,app.variables.json,pages-config.json]

--- src/main/webapp/pages/TestPage3/TestPage3.js
+++ src/main/webapp/pages/TestPage3/TestPage3.js
@@ -1,29 +1,24 @@
-Application.$controller("MainPageController", ["$scope", function($scope) {
+Application.$controller("TestPage3PageController", ["$scope", function ($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
-    $scope.onPageReady = function() {
+    $scope.onPageReady = function () {
         /*
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
          * $scope.Variables.loggedInUser.getData()
-         * 
+         *
          * widgets can be accessed through '$scope.Widgets' property here
          * e.g. to get value of text widget named 'username' use following script
          * '$scope.Widgets.username.datavalue'
          */
     };
 
-
-    $scope.button8Click = function($event, $isolateScope) {
-        $scope.Widgets.EmployeeTable1.selecteditem = undefined;
-    };
-
 }]);
 
-Application.$controller("EmployeeTable1Controller", ["$scope",
-    function($scope) {
-        "use strict";
-        $scope.ctrlScope = $scope;
-    }
+Application.$controller("UserTable1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
 ]);
