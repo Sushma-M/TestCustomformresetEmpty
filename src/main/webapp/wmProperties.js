92be14cad59187435534dc6822c08ee502cf1e61
Sushma Wm <sushma.maddirala@wavemaker.com>
2017-10-05 09:14:04 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "TestCustomformresetEmpty",
+  "homePage" : "Main",
+  "name" : "TestCustomformresetEmpty",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
